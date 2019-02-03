c05237db301333b04ddf3dab1407c5cc298f51d3
zcabsva <zcabsva@ucl.ac.uk>
2019-02-03 19:35:07 +0000

    Testing (minor fixes)

--- src/__tests__/Editor.test.js
+++ src/__tests__/Editor.test.js
@@ -4,12 +4,12 @@ import Editor from '../LabView/Editor';
 import renderer from "react-test-renderer";
 
 describe("Editor Component Test", () => {
-  it("Renders without crashing", () => {
+  it("Editor renders without crashing", () => {
     const div = document.createElement('div');
     ReactDOM.render(<Editor />, div);
   });
 
-  it("Renders correctly according to first snapshot", () => {
+  it("Editor renders correctly according to first snapshot", () => {
     const tree = renderer.create(<Editor />).toJSON();
     expect(tree).toMatchSnapshot();
   });
--- src/__tests__/LabView.test copy.js
+++ src/__tests__/LabView.test copy.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import LabView from '../LabView/LabView';
 import renderer from "react-test-renderer";
 
-describe("Editor Component Test", () => {
-  it("Renders without crashing", () => {
+describe("LabView Component Test", () => {
+  it("LabView renders without crashing", () => {
     const div = document.createElement('div');
     ReactDOM.render(<LabView />, div);
   });
 
-  it("Renders correctly according to first snapshot", () => {
+  it("LabView renders correctly according to first snapshot", () => {
     const tree = renderer.create(<LabView />).toJSON();
     expect(tree).toMatchSnapshot();
   });
