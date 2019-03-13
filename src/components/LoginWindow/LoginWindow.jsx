47d5bca0e9a0fbdb7fdfcfc4e78ee1decd9c9b13
Adam Peace <adam.n.peace@gmail.com>
2019-03-13 16:20:21 +0000

    Add advanced form validation and remove warning for uncontrolled input

--- src/components/LoginWindow/LoginWindow.jsx
+++ src/components/LoginWindow/LoginWindow.jsx
@@ -7,11 +7,13 @@ import useForm from '../../helpers/useForm';
 import { withFirebase } from '../../Auth/Firebase';
 
 import './LoginWindow.scss';
+import validate from './LoginFormValidationRules';
 
 const propTypes = {
   firebase: PropTypes.object.isRequired,
   label: PropTypes.string,
   redirectLink: PropTypes.string,
+  history: PropTypes.object.isRequired,
 };
 const defaultProps = {
   label: 'Log In',
@@ -19,8 +21,9 @@ const defaultProps = {
 };
 
 function LoginWindow(props) {
-  const { values, handleChange, handleSubmit } = useForm(signin);
-  const { firebase, label, redirectLink } = props;
+  // eslint-disable-next-line
+  const { errors, values, handleChange, handleSubmit } = useForm(signin, validate);
+  const { firebase, history, label, redirectLink } = props;
 
   async function signin() {
     const { email, password } = values;
@@ -28,28 +31,39 @@ function LoginWindow(props) {
       .doSignIn(email, password)
       .then(() => {
         notify.show('You have been logged in successfully!', 'success');
-        props.history.replace(redirectLink);
+        history.replace(redirectLink);
       })
-      .catch(error => notify.show(error.message, 'error'));
+      .catch(error => {
+        if (
+          error.message ===
+            'There is no user record corresponding to this identifier. The user may have been deleted.' ||
+          error.message === 'The password is invalid or the user does not have a password.'
+        ) {
+          notify.show('Your login credentials are invalid', 'error');
+        } else {
+          notify.show(error.message, 'error');
+        }
+      });
   }
 
   return (
     <div className="box form-card">
       <p className="label has-text-centered has-text-weight-semibold is-size-4">{label}</p>
       <br />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} noValidate>
         <div className="field">
-          <label className="label">Email Address</label>
+          <p className="label">Email Address</p>
           <div className="control">
             <input
               aria-label="email"
-              className="input"
+              className={`input ${errors.email && 'is-danger'}`}
               type="email"
               name="email"
               onChange={handleChange}
               value={values.email}
               required
             />
+            {errors.email && <p className="help is-danger">{errors.email}</p>}
           </div>
         </div>
         <div className="field">
@@ -57,13 +71,14 @@ function LoginWindow(props) {
           <div className="control">
             <input
               aria-label="password"
-              className="input"
+              className={`input ${errors.password && 'is-danger'}`}
               type="password"
               name="password"
               onChange={handleChange}
               value={values.password}
               required
             />
+            {errors.password && <p className="help is-danger">{errors.password}</p>}
           </div>
         </div>
         <br />
